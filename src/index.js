aab315b7b790258833bd02b5742b27eb1da0a1eb
JxAmani <jeremyamani336@gmail.com>
2025-06-25 16:16:18 +0300

    Updating index.js

--- src/index.js
+++ src/index.js
@@ -8,5 +8,23 @@ function main() {
   setupAddPostButton();
   handleNewPostSubmit();
 }
+function displayPosts() {
+  fetch(BASE_URL)
+    .then(res => res.json())
+    .then(posts => {
+      const postList = document.getElementById("post-list");
+      postList.innerHTML = posts.map(post =>
+        `<div class="post-item" data-id="${post.id}">${post.title}</div>`
+      ).join("");
+      // Attach event listeners after rendering
+      document.querySelectorAll(".post-item").forEach(item => {
+        item.addEventListener("click", () => {
+          const id = item.dataset.id;
+          const post = posts.find(p => p.id == id);
+          showPostDetails(post);
+        });
+      });
+    });
+}
 
 
