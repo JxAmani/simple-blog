af607d4d9c18d0356848c6512382c9ba779f1ce5
JxAmani <jeremyamani336@gmail.com>
2025-06-25 15:31:31 +0300

    Updating index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+const BASE_URL = "http://localhost:3000/posts";
+
+// Run once DOM is loaded
+document.addEventListener("DOMContentLoaded", main);
+
+function main() {
+  displayPosts();
+  setupAddPostButton();
+  handleNewPostSubmit();
+}
 
 
